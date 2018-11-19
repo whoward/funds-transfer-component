4636931636d1e9d39c54914ca842de9a0880043c
William Howard <will@influitive.com>
2018-11-19 00:21:24 -0500

    adds the deposited event

---
+++ lib/funds_transfer_component/messages/events/deposited.rb
@@ -0,0 +1,16 @@
+module FundsTransferComponent
+  module Messages
+    module Events
+      class Deposited
+        include Messaging::Message
+
+        attribute :funds_transfer_id, String
+        attribute :deposit_id, String
+        attribute :account_id, String
+        attribute :amount, Numeric
+        attribute :time, String
+        attribute :processed_time, String
+      end
+    end
+  end
+end
